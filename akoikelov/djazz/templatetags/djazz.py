9526026b85fd41d21d632be9faaa4d318ecf1195
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-11-01 20:52:23 +0600

    + added static_info.css styles file

--- akoikelov/djazz/templatetags/djazz.py
+++ akoikelov/djazz/templatetags/djazz.py
@@ -4,7 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.inclusion_tag('static_info/js.html', takes_context=True)
+@register.inclusion_tag('static_info/constructor.html', takes_context=True)
 def include_si(context):
     if getattr(settings, 'SI_INCLUDE_JQUERY', False):
         context['include_jquery'] = True
